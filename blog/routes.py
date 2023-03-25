7f1dd85d541a76e6321e1ca5e63b3195bc723dba
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-03-25 16:54:08 +0100

    Improved pages structure

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,19 @@
 from blog import app
+from flask import render_template
 
 
 @app.route("/")
 def homepage():
-    return "<h1>Homepage<h1>"
+    posts = [
+        {'title': 'First post', 'body': 'First body'},
+        {'title': 'Second post', 'body': 'Second body'},
+    ]
+    some_boolean_flag = False
+
+    return render_template("homepage.html",
+                           posts=posts, boolean_flag=some_boolean_flag)
+
+
+@app.route("/about")
+def about():
+    return render_template("aboutpage.html")
