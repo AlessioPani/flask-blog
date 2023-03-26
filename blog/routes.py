f9ea2f2086318042f1ca937f3403c8a2433e9f10
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-03-26 19:25:38 +0200

    Added page for single post and new related view

--- blog/routes.py
+++ blog/routes.py
@@ -6,8 +6,14 @@ from flask import render_template
 @app.route("/")
 def homepage():
     posts = Post.query.order_by(Post.created_at.desc()).all()
-    
+
     return render_template("homepage.html", posts=posts)
+
+
+@app.route('/post/<int:post_id>')
+def post_detail(post_id):
+    post_instance = Post.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post_instance)
 
 
 @app.route("/about")
