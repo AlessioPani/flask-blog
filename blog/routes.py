6c1d298436ff9d22d7c857d29184ca43dc4260ca
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-03-28 18:49:58 +0200

    Fixed redirect for wrong credentials

--- blog/routes.py
+++ blog/routes.py
@@ -32,7 +32,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password, try again.')
-            return redirect('login')
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('homepage'))
     return render_template('login.html', form=form)
